3d8867e5eb280dc8c4b58034e865afb9e96881b2
Мария Тарлавина <tarlavina50@mail.ru>
2024-07-04 19:18:15 +0700

    process work

---
+++ GeometricShape/ConsoleRenderer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricShape
+{
+    internal interface ConsoleRenderer : IRenderer
+    {
+        public void RenderToScreen(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public void RenderToFile(string text)
+        {
+            using (StreamWriter writer = new StreamWriter("output.txt", true))
+            {
+                writer.WriteLine(text);
+            }
+        }
+    }
+}
---
+++ GeometricShape/IRenderer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricShape
+{
+    internal interface IRenderer
+    {
+        void RenderToScreen(string text);
+        void RenderToFile(string text);
+    }
+}
