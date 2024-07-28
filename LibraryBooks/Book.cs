c5dbeb24adabd19ab0652a3dbbfe080e4559d442
Мария Тарлавина <tarlavina50@mail.ru>
2024-07-28 21:31:31 +0700

    Index готов, превая страница со списком

---
+++ LibraryBooks/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace LibraryBooks
+{
+    public class Book
+    {
+        public string Title { get; set; } // название
+        public string Author { get; set; } // автор
+        public string Genre { get; set; } // жанр
+        public string Publisher { get; set; } // издательство
+        public int YearPublished { get; set; } // год издания
+    }
+}
--- LibraryBooks/Pages/Index.cshtml.cs
+++ LibraryBooks/Pages/Index.cshtml.cs
@@ -10,16 +10,18 @@ namespace LibraryBooks.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+        public List<Book> Books { get; set; }
 
         public void OnGet()
         {
-
+            Books = new List<Book>
+            {
+                new Book { Title = "1984", Author = "Джордж Оруэлл", Genre = "Научная фантастика", Publisher = "Secker & Warburg", YearPublished = 1949 },
+                new Book { Title = "Убить пересмешника", Author = "Харпер Ли", Genre = " Роман", Publisher = "J. B. Lippincott & Co.", YearPublished = 1960 },
+                new Book { Title = "Великий Гэтсби", Author = "Фрэнсис Скотт Фицджеральд", Genre = "Роман", Publisher = "Charles Scribner's Sons", YearPublished = 1925 },
+                new Book { Title = "Сто лет одиночества", Author = "Габриэль Гарсиа Маркес", Genre = " Магический реализм", Publisher = "Editorial Sudamericana", YearPublished = 1967 },
+                new Book { Title = "Дом, в котором...", Author = "Мариам Петросян", Genre = "Мистика", Publisher = "Издательство Эксмо", YearPublished = 2012 }
+            };
         }
     }
 }
