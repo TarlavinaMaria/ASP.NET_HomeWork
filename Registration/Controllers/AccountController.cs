2d0c10a0321b3b3a67cd7e7686c090d19b23bf72
Мария Тарлавина <tarlavina50@mail.ru>
2024-07-31 16:27:05 +0700

    добавлнена страница регистрации и контролер

---
+++ Registration/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Registration.Models;
+
+namespace Registration.Controllers
+{
+    public class AccountController : Controller
+    {
+        // Действие для отображения формы регистрации
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View(new RegisterModel());
+        }
+
+        // Действие для обработки данных регистрации
+        [HttpPost]
+        public ActionResult Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Проверка данных пользователя и регистрация
+                // ...
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                // Данные пользователя некорректны, отображаем форму снова
+                return View(model);
+            }
+        }
+    }
+}
---
+++ Registration/Models/RegisterModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Registration.Models
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Имя пользователя обязательно")]
+        [Display(Name = "Имя пользователя")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Email обязателен")]
+        [EmailAddress(ErrorMessage = "Неверный формат email")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Пароль обязателен")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Подтверждение пароля обязательно")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Подтверждение пароля")]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        public string ConfirmPassword { get; set; }
+    }
+}
